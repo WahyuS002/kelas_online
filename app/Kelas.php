f7635a5e1229054d81e8979de70f80e4f2d50344
WahyuS002 <syahputrawahyu61@gmail.com>
2020-08-28 06:35:16 +0700

    remove model & migration from tingkatan, level, mapel

--- app/Kelas.php
+++ app/Kelas.php
@@ -10,21 +10,6 @@ class Kelas extends Model
 
     protected $fillable = ['user_id', 'kategori_id', 'jenjang', 'level', 'nama_kelas', 'slug_kelas', 'thumbnail', 'deskripsi', 'harga', 'diskon', 'durasi_kelas', 'kapasitas_kelas', 'video_preview'];
 
-    public function levelKelas()
-    {
-        return $this->belongsTo(LevelKelas::class, 'level_kelas_id');
-    }
-
-    public function mapelKelas()
-    {
-        return $this->belongsTo(Mapel::class, 'mapel_kelas_id');
-    }
-
-    public function tingkatanKelas()
-    {
-        return $this->belongsTo(TingkatanKelas::class, 'tingkatan_kelas_id');
-    }
-
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
