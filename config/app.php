9cd4752067521a2423cc116b1a8ba0e3be4309ff
WahyuS002 <syahputrawahyu61@gmail.com>
2020-09-08 08:57:12 +0700

    deleting multi auth package

--- config/app.php
+++ config/app.php
@@ -161,6 +161,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+
+        /*
+         * Custom Package...
+         */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
 
         /*
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-use App\Kelas;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
