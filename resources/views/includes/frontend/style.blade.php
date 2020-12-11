c389a22b6d39750df32f728048897a342479c032
WahyuS002 <syahputrawahyu61@gmail.com>
2020-12-11 21:47:31 +0700

    search

--- resources/views/includes/frontend/style.blade.php
+++ resources/views/includes/frontend/style.blade.php
@@ -2,6 +2,10 @@
 <link href="{{ asset('cork-frontend/assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
 <script src="{{ asset('cork-frontend/assets/js/loader.js') }}"></script>
 
+<!-- BEGIN MY STYLES -->
+<link rel="stylesheet" href="{{ asset('css/frontend/search-dropdown.css') }}">
+<!-- END MY STYLES -->
+
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
 <link href="{{ asset('cork/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
--- resources/views/livewire/search/kelas-search.blade.php
+++ resources/views/livewire/search/kelas-search.blade.php
@@ -2,15 +2,37 @@
     <input type="text" class="form-control search-form-control  ml-lg-auto" placeholder="Search..." wire:model="query">
 
     @if ($query)
-        @if ($kelas)
-            <div class="position-absolute z-10 bg-white w-full shadow-lg mt-1">
-                @foreach ($kelas as $k)
-                <a href="#">{{ $k['nama_kelas'] }}</a>
-                @endforeach
+    <div class="search-dropdown">
+        <div class="row">
+            @if ($kelas)
+            @foreach ($kelas as $k)
+            <div class="col-1 mr-2">
+                <img src="{{ asset('storage/kelas/laravel.png') }}" width="27" height="27">
             </div>
-        @else
-            <div class="position-absolute z-10 list-item bg-white w-full">No Result</div>
-        @endif
+            <div class="col-10">
+                <a href="kelas/{{ $k['slug_kelas'] }}/detail">
+                    {{ $k['nama_kelas'] }}
+                </a>
+            </div>
+            @endforeach
+            @else
+            <p>No Result</p>
+            @endif
+        </div>
+    </div>
     @endif
 
-</div>
+
+    {{-- @if ($query)
+        @if ($kelas)
+        <div class="position-absolute z-10 bg-white w-full shadow-lg mt-1">
+            @foreach ($kelas as $k)
+            <a href="#">{{ $k['nama_kelas'] }}</a>
+            @endforeach
+        </div>
+        @else
+        <div class="position-absolute z-10 list-item bg-white w-full">No Result</div>
+        @endif
+        @endif --}}
+
+    </div>
